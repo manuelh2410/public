f806860aad81684695577c01be7955e308b184e3
manuelh2410 <manuel_hubbard@hotmail.com>
2021-10-13 09:36:22 +0200

    logging typo

--- AWSIP_Linux_Win.ps1
+++ AWSIP_Linux_Win.ps1
@@ -224,7 +224,7 @@ Write-host "START Instance vs SG Group evaluation"
     {
       IF(((Get-EC2Instance -InstanceId $instance).Instances.securitygroups.GroupName -notcontains "RDP") -or  ((Get-EC2Instance -InstanceId $instance).Instances.securitygroups.GroupName -notcontains "SSH"))
 
-        { write-host Evanluating Groups for instance $instance
+        { write-host Evaluating Groups for instance $instance
           Do
             {
 
